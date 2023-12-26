dcc05f9a26b8a1d15c2740cb6b06e3f5b518086a
Manish K <manishkuniyal24@gmail.com>
2023-12-26 11:38:26 +0530

    fixedDeployementError

--- src/app/components/header.jsx
+++ src/app/components/header.jsx
@@ -9,8 +9,11 @@ function Header() {
   return (<>
   <div className=' p-3 lg:px-[20%] px-[7%] flex justify-between items-center'>
     <Link href='/' className=' text-[1.3rem]'>neXshop</Link>
-    <Link href='/cart' className='font-bold text-[1rem] '>
-      <h1>Cart</h1> 
+    <Link href='/cart' className=' font-bold text-[1rem] '>
+      <h1 className=' px-2 rounded-[10px] bg-white' >Cart</h1> 
+      {cartItems.length > 0 && (
+  <span className='border-white absolute top-4  w-[10px] right-[6%] lg:right-[19.5%] bg-blue-400 h-[10px] rounded-[50%] animate-ping'></span>
+          )}
     </Link>
     </div>   
     </>
